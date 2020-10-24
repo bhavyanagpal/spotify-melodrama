77a3df370cbe4fd6496cd7067b5eb73c320d2385
Bhavya <53962820+bhavyanagpal@users.noreply.github.com>
2020-10-24 18:08:47 +0530

    Update lyrics.py

--- lyrics.py
+++ lyrics.py
@@ -22,6 +22,7 @@ import lyricsgenius
 import types
 import matplotlib.pyplot as plt
 import seaborn as sns
+import mpld3
 color = sns.color_palette()
 genius = lyricsgenius.Genius('l4J-3WFatATy8mJHgnsYVausDrWZo9GsXzui4_DWu5PiXfMDZ_s30iAB2DGtdEMQ')
 # for acessing private playlists
@@ -48,73 +49,77 @@ token = util.prompt_for_user_token(
     client_id='3cb41450f466404399f3e0de3e4c89f2',
     client_secret='51406619960a46c3a0fc8682e5152784',
     redirect_uri='http://localhost:8888/callback/')
+def initial():
+    spotify = spotipy.Spotify(auth=token)
+    spotify.current_user_recently_played = types.MethodType(current_user_recently_played, spotify)
 
-spotify = spotipy.Spotify(auth=token)
-spotify.current_user_recently_played = types.MethodType(current_user_recently_played, spotify)
 
 # creating .json file
-recentsongs = spotify.current_user_recently_played(limit=50)
-track_name = []
-album_name = []
-artist_name = []
+
+    recentsongs = spotify.current_user_recently_played(limit=10)
+    track_details = []
 # creating arrays for storing id and name
-for i in recentsongs['items']:
-    track_name.append(i['track']['name'])
-    artist_name.append(i['track']['artists'][0]['name'])
-lyrics = {}
-text = []
-compoundscore = []
-sid = SentimentIntensityAnalyzer()
-track_name = list(dict.fromkeys(track_name))
-for i in range(len(track_name)):
-    song = genius.search_song(track_name[i], artist_name[i])
-    songlyrics = song.lyrics.replace("\n", " ").replace("\\'", "\'")
-    lyrics[track_name[i]] = songlyrics
-    songlyrics = songlyrics.replace('(', '').replace(')', '')
-    text.append(re.sub("[\\[].*?[\\]]", "", songlyrics))
-    scores = sid.polarity_scores(text[i])
-    compoundscore.append(scores['compound'])
-text = ' '.join(map(str, text))
-print(text.encode("utf-8"))
-stopwords = set(stopwords.words('english'))
-stopwords.update(["br", "href", "la", "yeah", "yuh", "wan", "i'm"])
 
-sentences = sent_tokenize(text)
-words = word_tokenize(text)
-words_no_punc = []
-for w in words:
-    if w.isalpha():
-        words_no_punc.append(w.lower())
+    for i in recentsongs['items']:
+        temp = {'name': '', 'artist': ''}
+        temp['name'] = i['track']['name']
+        temp['artist'] = i['track']['artists'][0]['name']
+        track_details.append(temp)
+    lyrics = {}
+    text = []
+    compoundscore = []
+    sid = SentimentIntensityAnalyzer()
+    track_details = {frozenset(item.items()): item for item in track_details}.values()
+    print(track_details)
+    for i in track_details:
+        song = genius.search_song(i['name'], i['artist'])
+        songlyrics = song.lyrics.replace("\n", " ").replace("\\'", "\'")
+        lyrics[i['name']] = songlyrics
+        songlyrics = songlyrics.replace('(', '').replace(')', '')
+        songlyrics = re.sub("[\\[].*?[\\]]", "", songlyrics)
+        text.append(songlyrics)
+        scores = sid.polarity_scores(songlyrics)
+        compoundscore.append(scores['compound'])
+    text = ' '.join(map(str, text))
+    print(text.encode("utf-8"))
+    stpwords = set(stopwords.words('english'))
+    stpwords.update(["br", "href", "la", "yeah", "yuh", "wan", "i'm"])
 
-ps = PorterStemmer()
-clean_words = []
-for w in words_no_punc:
-    if w not in stopwords:
-        clean_words.append(ps.stem(w))
-fdist = FreqDist(clean_words)
-print(fdist.most_common(10))
-fdist.plot(10)
-def test():
-    print("HEy")
+    sentences = sent_tokenize(text)
+    words = word_tokenize(text)
+    words_no_punc = []
+    for w in words:
+        if w.isalpha():
+            words_no_punc.append(w.lower())
 
-# making wordcloud
-words_string = ' '.join(map(str, clean_words))
-wordcloud = WordCloud(stopwords=stopwords).generate(words_string)
-plt.imshow(wordcloud, interpolation='bilinear')
-plt.axis("off")
-plt.show()
-print(compoundscore)
-plt.plot(compoundscore)
-plt.show()
-pos_count = 0
-neg_count = 0
-for num in compoundscore:
-    if num >= 0:
-        pos_count += 1
+    ps = PorterStemmer()
+    clean_words = []
+    for w in words_no_punc:
+        if w not in stpwords:
+            clean_words.append(ps.stem(w))  
+    fdist = FreqDist(clean_words)
+    print(fdist.most_common(10))
+    fdist.plot(10)
+    words_string = ' '.join(map(str, clean_words))
+    wordcloud = WordCloud(stopwords=stpwords).generate(words_string)
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.axis("off")
+    p1=plt.show()
+    print(compoundscore)
+    plt.plot(compoundscore)
+    p2=plt.show()
+    pos_count = 0
+    neg_count = 0
+    for num in compoundscore:
+        if num >= 0:
+            pos_count += 1
 
-    else:
-        neg_count += 1
-plt.pie([pos_count, neg_count], labels=["Positive Songs", "Negative Songs"])
+        else:
+            neg_count += 1
+    plt.pie([pos_count, neg_count], labels=["Positive Songs", "Negative Songs"])
+    p3=plt.show()
+    p1
+    p2
+    p3
 
-def lyl():
-    return plt.show()
+
